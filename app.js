1e0af64e6d93bb48f4704913383448842f87de60
JR Charles <jrcharles@mac.com>
2020-07-06 19:01:13 -0400

    Lecture 32 - Challenge

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=805308cf562392412b4ed958fc5acd87&query=Knoxville'
+const url = 'http://api.weatherstack.com/current?access_key=805308cf562392412b4ed958fc5acd87&query=Knoxville,%20TN&units=f'
 
 request({
     url: url,
     json: true
 }, (err, res) => {
     const data = res.body.current
-    console.log('It is currently ' + data.temperature + ' degrees.  It feels like ' + data.feelslike + ' out.')
+    console.log(data.weather_descriptions[0] + '. It is currently ' + data.temperature + ' degrees.  It feels like ' + data.feelslike + ' out.')
 })
