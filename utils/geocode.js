6acc8a088587a93ab689b1d339a04bef452548f3
JR Charles <jrcharles@mac.com>
2020-07-06 22:48:34 -0400

    Leture 36 = Geocode callback

---
+++ utils/geocode.js
@@ -0,0 +1,25 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const api = 'pk.eyJ1IjoianJjaGFybGVzIiwiYSI6ImNrY2I0MXcxcDFuOXYyd3FwOHM4OHdkdmQifQ.iFqLmXKZMeKfHh9VQK8t3g'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + api + '&limit=1'
+
+    request({
+        url: url,
+        json: true
+    }, (err, res) => {
+        if (err) {
+            callback('Unable to connect to geocoding service', undefined)
+        } else if (res.body.features.length === 0) {
+            callback('Unable to find location.  Try changing your search terms', undefined)
+        } else {
+            callback(undefined, {
+                long: res.body.features[0].center[0],
+                lat: res.body.features[0].center[1],
+                location: res.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
