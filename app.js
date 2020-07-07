bed0ae7a1e28bda3d3a87954cbef211bff366528
JR Charles <jrcharles@mac.com>
2020-07-06 21:03:33 -0400

    Lecture 33 - pull Mapbox API

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=805308cf562392412b4ed958fc5acd87&query=Knoxville,%20TN&units=f'
-
 request({
-    url: url,
+    url: 'http://api.weatherstack.com/current?access_key=805308cf562392412b4ed958fc5acd87&query=Knoxville,%20TN&units=f',
     json: true
 }, (err, res) => {
     const data = res.body.current
     console.log(data.weather_descriptions[0] + '. It is currently ' + data.temperature + ' degrees.  It feels like ' + data.feelslike + ' out.')
+})
+
+request({
+    url: 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoianJjaGFybGVzIiwiYSI6ImNrY2I0MXcxcDFuOXYyd3FwOHM4OHdkdmQifQ.iFqLmXKZMeKfHh9VQK8t3g&limit=1',
+    json: true
+}, (err, res) => {
+    const long = res.body.features[0].center[0]
+    const lat = res.body.features[0].center[1]
 })
